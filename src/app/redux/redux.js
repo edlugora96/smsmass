b9092c0222182a565ff68007b6d67d461274802c
Eduardo <edlu.gora96@gmail.com>
2019-02-05 11:23:11 -0400

    Acomodos para no volver a incurrir en los errores anteriores smsHanddlerServer

--- src/app/redux/redux.js
+++ src/app/redux/redux.js
@@ -1,8 +1,5 @@
 import { createReducer, createAction } from 'redux-starter-kit'
 
-export const increment = createAction('counter/increment')
-export const decrement = createAction('counter/decrement')
-
 export const saveTable = createAction('table/savecontacts')
 
 export const saveTableReducer = createReducer(
@@ -11,12 +8,6 @@ export const saveTableReducer = createReducer(
   [saveTable]: (state, action) => state = action.payload
 })
 
-const counterReducer = createReducer(0, {
-  [increment]: (state, action) => state + action.payload,
-  [decrement]: (state, action) => state - action.payload
-})
-
 export default {
-  saveTableReducer,
-  counterReducer
+  saveTableReducer
 };
