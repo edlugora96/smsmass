4f3474441b5ec648901a1e3b3a2dc35a7a3f1fbb
Eduardo <edlu.gora96@gmail.com>
2019-02-08 21:51:42 -0400

    Start PDU

--- src/server/controllers/handlerSmsObj.js
+++ src/server/controllers/handlerSmsObj.js
@@ -1,7 +1,7 @@
 const SerialPort = require ( 'serialport' );
 let pdu = require('pdu');
 const Port = require('serial-at');
-/*const portConst = new Port("COM8", {
+const portConst = new Port("COM8", {
      baudRate: 9600,  
      dataBits: 8,  
      parity: 'none',  
@@ -12,18 +12,17 @@ const Port = require('serial-at');
      xoff:false, 
      xany:false, 
      buffersize:0
-})*/
-// const portConst = new SerialPort();
+})
 class handlerSms 
 {
   constructor(mg, ph) 
   {
-    // this.port = portConst;
+    this.port = portConst;
     this.phone = ph;
     this.message = mg;
     this.response;
   }
-/*  read()
+  read()
   {
     console.log('port communication open');
     this.port.write("AT+CMGF=?" );
@@ -35,18 +34,18 @@ class handlerSms
       return this.response;
     } )  
   }
-*/
+
   send()
   {
     if (this.phone!= '' && this.message!= '') 
     {
       let pduSMS = pdu.generate({
-        text:this.message,
         receiver:this.phone, //MSISDN
+        text:this.message,
         encoding:'16bit' //Or 7bit if you're sending an ascii message.
       });
-      console.log(pduSMS)
-      /*return this.port.at("AT+CMGF=0\rAT+CMGS=42\""+pduSMS+'^z').then(
+      // console.log(pduSMS)
+      return this.port.at("AT+CMGF=0\rAT+CMGS=00 41 "+pduSMS+'^z').then(
         e=>{
           if (String(e).indexOf('ERROR')!==-1) 
             {
@@ -59,14 +58,14 @@ class handlerSms
             }
       ).catch(
         e=> {throw new Error(e)}
-      )*/
+      )
     }
   }
 
   setData()
   {
     var promiseGetData = new Promise( (resolve, reject) => { 
-      // this.port.open().then(e=>console.log('Port are open sussces')).catch(e=>console.log('Error to try open the port '+e))
+      this.port.open().then(e=>console.log('Port are open sussces')).catch(e=>console.log('Error to try open the port '+e))
     })
     return promiseGetData
   }
