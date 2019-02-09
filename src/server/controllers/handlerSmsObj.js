fd2abf03a36d6f2c989a42ecfe237be99b2c6dfd
Eduardo <edlu.gora96@gmail.com>
2019-02-09 15:06:10 -0400

    CHECKPOINT

--- src/server/controllers/handlerSmsObj.js
+++ src/server/controllers/handlerSmsObj.js
@@ -1,28 +1,28 @@
 const SerialPort = require ( 'serialport' );
-let pdu = require('pdu');
+// let pdu = require('pdu');
 const Port = require('serial-at');
-/*const portConst = new Port("COM8", {
-     baudRate: 9600,  
-     dataBits: 8,  
-     parity: 'none',  
-     stopBits: 1, 
-     flowControl: false, 
-     xon : false, 
-     rtscts:false, 
-     xoff:false, 
-     xany:false, 
+const portConst = new Port("COM8", {
+     baudRate: 9600,
+     dataBits: 8,
+     parity: 'none',
+     stopBits: 1,
+     flowControl: false,
+     xon : false,
+     rtscts:false,
+     xoff:false,
+     xany:false,
      buffersize:0
-})*/
-class handlerSms 
+})
+class handlerSms
 {
-  constructor(mg, ph) 
+  constructor(mg, ph)
   {
-    // this.port = portConst;
+    this.port = portConst;
     this.phone = ph;
     this.message = mg;
     this.response;
   }
-  /*read()
+  read()
   {
     console.log('port communication open');
     this.port.write("AT+CMGF=?" );
@@ -32,36 +32,27 @@ class handlerSms
       console.log( "Received data: " + data )
       this.response = data;
       return this.response;
-    } )  
-  }*/
+    } )
+  }
 
   send()
   {
-    if (this.phone!= '' && this.message!= '') 
+    if (this.phone!= '' && this.message!= '')
     {
-      var promiseGetData = new Promise( (resolve, reject) => { 
-        resolve('OK')
-      })
-      return promiseGetData
-      /*let pduSMS = pdu.generate({
-        text:this.message,
-        receiver:this.phone, //MSISDN
-        encoding:'16bit' 
-      });
-      return this.port.at("AT+CMGF=0\rAT+CMGS=00 41 "+pduSMS+'^z').then(
+      return this.port.at("AT+CMGF=1\rAT+CMGS=\""+this.phone+"\""+this.message+'^z').then(
         e=>{
-          if (String(e).indexOf('ERROR')!==-1) 
+          if (String(e).indexOf('ERROR')!==-1)
             {
               throw e
-            } 
-            else 
-              { 
-                return e 
+            }
+            else
+              {
+                return e
               }
             }
       ).catch(
         e=> {throw new Error(e)}
-      )*/
+      )
     }
   }
 
